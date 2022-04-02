9dadd7617c71f216307a576204a904b7874b6d54
加藤義隆 <991024yoshi@gmail.com>
2022-04-02 23:55:02 +0900

    add styles

---
+++ js/main.js
@@ -0,0 +1,18 @@
+'use strict'
+
+{
+    const year = 2020;
+    const month = 4;
+
+    function getCalendarBody() {
+        const dates = [];
+        const lastDate = new Date(year, month + 1, 0).getDate();
+
+        for (let i = 1; i <= lastDate; i++) {
+            dates.push(i);
+        }
+
+        console.log(dates);
+    }
+    getCalendarBody();
+}
