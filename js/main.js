9b51848b0d7339f0a4edb77539174c47d2327f38
加藤義隆 <991024yoshi@gmail.com>
2022-04-03 00:34:26 +0900

    add class action

--- js/main.js
+++ js/main.js
@@ -4,15 +4,36 @@
     const year = 2020;
     const month = 4;
 
+    function getCalendarHead() {
+        const dates = [];
+        const d = new Date(year, month, 0).getDate();
+        const n = new Date(year, month, 1).getDay();
+
+        for (let i = 0; i < n; i++) {
+            dates.unshift({
+                date: d- i,
+                isToday: false,
+                isDisabled: true,
+            });
+        }
+
+        console.log(dates);
+    }
+
     function getCalendarBody() {
         const dates = [];
         const lastDate = new Date(year, month + 1, 0).getDate();
 
         for (let i = 1; i <= lastDate; i++) {
-            dates.push(i);
+            dates.push({
+                date: i,
+                isToday: false,
+                isDisabled: false,
+            });
         }
 
         console.log(dates);
     }
-    getCalendarBody();
+    // getCalendarBody();
+    getCalendarHead();
 }
